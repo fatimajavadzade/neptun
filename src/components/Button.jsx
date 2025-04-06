c91c1dc4235028e1003147e42139a475fcf648e6
Nurlan Askerov <nurlaneskerov000@gmail.com>
2025-04-07 00:23:18 +0400

    config file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ label, onClick }) {
     return (
       <button
         onClick={onClick}
-         className="px-8 py-3 font-semibold rounded-full cursor-pointer bg-[#ff8300] text-white"
+         className="px-8 py-3 font-semibold rounded-full cursor-pointer bg-primary text-white"
       >
         {label}
       </button>
