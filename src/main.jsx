c4b4f4775ef4c2e77b68c6598d653e76bbb02506
Nurlan Askerov <nurlaneskerov000@gmail.com>
2025-04-17 16:49:46 +0400

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Test from './components/Test.jsx'
-import ToDo from './components/ToDo.jsx'
+
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
          <App /> 
-         {/* <Test/>  */}
-         {/* <ToDo/> */}
     </BrowserRouter>
 )
